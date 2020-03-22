7479aa50d87a1c409840086d0efa86b63459da4b
Artem Onuchin <art5654@yandex.ru>
2020-03-22 19:59:28 +0300

    Add PRICE RANGE. But I don't make a normal display this block:(

--- js/control.js
+++ js/control.js
@@ -1,11 +1,30 @@
-$(function(){
+$(function () {
 
     "use strict";
-    
-    $('.title__box').click(function(){
+
+    $('.title__box').click(function () {
 
         $(this).toggleClass('open');
         $(this).next('div.list__link').toggleClass('open');
     });
+
+    $('.filter__slider-price').slider({
+        max: 1000,
+        min: 0,
+        range: true,
+        values: [190, 728],
+        slide: function (event, ui) {
+            $('input[name="minPrice"]').val( '$' + 
+            ui.values[0]);
+            $('input[name="maxPrice"]').val( '$' + 
+            ui.values[1]);
+        }
+    });
+
+    $('input[name="minPrice"]').val( '$' + 
+        $('.filter__slider-price').slider('values', 0) );
+    $('input[name="maxPrice"]').val(  '$' + 
+        $('.filter__slider-price').slider('values', 1) );
+
 
 });
