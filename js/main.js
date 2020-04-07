30c772171debc338632faec0d931d082f39e0170
Artem Onuchin <art5654@yandex.ru>
2020-04-07 12:04:57 +0300

    Adapted to 1120

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@
 
 'use strict';
 
-let cartBox = document.querySelector ('.cartbox');
-let headerCart = document.querySelector ('.header__cart');
+// let cartBox = document.querySelector ('.cartbox');
+// let headerCart = document.querySelector ('.header__cart');
+// let cartboxClose = document.querySelector ('.cartbox_close');
 
-headerCart.addEventListener ('click', function () {
-    cartBox.classList.remove('hidden');
-});
+// headerCart.addEventListener ('click', function () {
+//     cartBox.classList.remove('hidden');
+// });
+
+
+// Почему-то не отрабатывает...
+
+// cartboxClose.addEventListener ('click', function () {
+//     console.log ('hihih');
+//     cartBox.classList.add('hidden');
+    
+// });
